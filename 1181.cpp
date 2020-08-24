200e3f41197fac524e59414adbcb0ece3aa7a168
muzy <sub06038@naver.com>
2020-08-24 18:24:01 +0900

    vector sort,unique,iter 연습

--- 1181.cpp
+++ 1181.cpp
@@ -4,43 +4,37 @@
 #include<algorithm>
 using namespace std;
 
+bool comp(const string& s1, const string& s2) {
+	if (s1.size() == s2.size()) {
+		return s1 < s2;    //s1이 s2보다 사전순으로 앞이면 true == 위치변환 x
+	}
+	return s1.size() < s2.size();
+}
+
 int main() {
+
+	vector<string> v;
 
 	int N;
 	cin >> N;
 
-	vector<string> word(N);
+	string str;
 
-	string temp;
 	for (int i = 0; i < N; i++) {
-		cin >> word[i];
+
+		cin >> str;
+		v.push_back(str);
 	}
 
-	for (int i = 0; i < N - 1; i++){
-		for (int j = 0; j < N - i - 1; j++) {
-			if (word[j].length() > word[j + 1].length()) {
-				temp = word[j];
-				word[j] = word[j + 1];
-				word[j + 1] = temp;
-			}
-			if (word[j].length() == word[j + 1].length() && word[j] > word[j+1]) {
-				temp = word[j];
-				word[j] = word[j + 1];
-				word[j + 1] = temp;
-			}
-			if (word[j] == word[j + 1])
-			{ 
-				word.erase(word.begin() + j);
-			}
-		}
+	vector<string>::iterator iter;
+	vector<string>::iterator end_iter;
+
+	sort(v.begin(), v.end(), comp);   //comp는 판단기준 boolean false면 자리 바꿈
+
+	end_iter = unique(v.begin(), v.end()); //겹치는거 걸러내고 마지막으로 주루룩 보내버림 쓰레기처리
+	                                       //ㄴ 그 보낸거에서 첫 번째 부분
+	for (iter = v.begin(); iter != end_iter; iter++) {
+		cout << *iter << endl;
 	}
-	
-
-
-	for (int i = 0; i < word.size(); i++) {
-		cout << word[i] << endl;
-	}
-	
-
 	return 0;
 }
